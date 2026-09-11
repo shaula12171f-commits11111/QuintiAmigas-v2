6c3b78d89e1bd35de1361ab4ef5c59b73ad1f471
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-11 14:56:26 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,13 @@
 // Este archivo está en .gitignore recomendado si pones keys reales.
 
 export const GROQ_KEYS = [
-  // "gsk_..."
+  "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",
+  "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",
+  "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",
+  "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",
+  "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"
+  
+
 ];
 
 export const MODELO = "openai/gpt-oss-120b";
