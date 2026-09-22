4476f26720d38dc85a6600f4ce7691b7f4c17d0f
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-22 16:47:53 -0500

    multi_hombres: nino_tiene_enfrente_dos_pollas

--- src/systems/imagenesMultiHombres.js
+++ src/systems/imagenesMultiHombres.js
@@ -16,6 +16,12 @@ const CHICAS_VALIDAS = ['Ichika', 'Nino', 'Miku', 'Yotsuba', 'Itsuki', 'Emilia']
 //  ESCENAS (agregá solo con URL real)
 // ---------------------------------------------------------------------------
 const ESCENAS = {
+  nino_tiene_enfrente_dos_pollas: {
+    url: 'https://raw.githubusercontent.com/shaula12171f-commits11111/wwasaxccvbiquintidfar/main/multi_hombres/nino_tiene_enfrente_dos_pollas.jpg',
+    descripcion: 'Nino mira dos pollas que estan frente a ella',
+    audio: ''
+  },
+
   nino_follando_en_doggy_y_chupando_polla: {
     url: 'https://img.ge/i/ssaly91.png',
     descripcion: 'Nino en doggy follando y chupando polla (multi hombres)',
