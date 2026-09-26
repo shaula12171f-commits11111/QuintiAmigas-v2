dc7bcab0c820822e0721ecf0175b9a038c95d27e
shaula12171f-commits11111 <shaula12171f@gmail.com>
2026-09-26 12:49:09 -0500

    parejas: nino_ichika_folladas_de_pie

--- src/systems/imagenesParejas.js
+++ src/systems/imagenesParejas.js
@@ -15,6 +15,12 @@ const CHICAS_VALIDAS = ['Ichika', 'Nino', 'Miku', 'Yotsuba', 'Itsuki', 'Emilia']
 //  ESCENAS (agregá solo con URL real)
 // ---------------------------------------------------------------------------
 const ESCENAS = {
+  nino_ichika_folladas_de_pie: {
+    url: 'https://raw.githubusercontent.com/shaula12171f-commits11111/wwasaxccvbiquintidfar/main/parejas/nino_ichika_folladas_de_pie.jpg',
+    descripcion: 'tienen camisas blancas y faldas verdes folladas al lado practicamente sus culos chocando entre si muy pegadas',
+    audio: ''
+  },
+
   nino_ichika_follando_en_el_aire: {
     url: 'https://raw.githubusercontent.com/SORFAR123123/Putas-de-fabri/main/imagenes/img_1772900802371.jpg',
     descripcion: 'Nino e Ichika follando en el aire con el usuario',
